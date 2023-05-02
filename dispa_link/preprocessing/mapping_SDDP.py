3070daedfbd4c5d9c6624e753bfe286aad5d3cd8
Matija Pavičević <31565754+MPavicevic@users.noreply.github.com>
2023-05-02 10:07:49 -0400

    fix(mapping): added missing scripts

---
+++ dispa_link/preprocessing/mapping_SDDP.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+
+def get_blocks_to_hours(start_date='2021-12-31 23:00:00+00:00', end_date='2026-12-31 23:00:00+00:00'):
+
+    df_t = pd.DataFrame()
+    df_t['Datetime'] = pd.date_range(start=start_date, end=end_date, freq='H')
+    df_t = df_t.assign(Weekday=pd.DatetimeIndex(df_t.Datetime).weekday, Hour=pd.DatetimeIndex(df_t.Datetime).hour, Year = pd.DatetimeIndex(df_t.Datetime).year, Week =pd.DatetimeIndex(df_t.Datetime).week)
+    df_t['Block'] = " "
+
+    condiciones =[
+        # lunes
+        (df_t['Weekday'] == 0) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] == 22),
+        (df_t['Weekday'] == 0) & (df_t['Hour'] >= 20) & (df_t['Hour'] <= 21),
+        # martes
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 22),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 20),
+        (df_t['Weekday'] == 1) & (df_t['Hour'] == 21),
+        # miercoles
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 22),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 20),
+        (df_t['Weekday'] == 2) & (df_t['Hour'] == 21),
+        # jueves
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 22),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 20),
+        (df_t['Weekday'] == 3) & (df_t['Hour'] == 21),
+        # viernes
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 22),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 20),
+        (df_t['Weekday'] == 4) & (df_t['Hour'] == 21),
+        # sabado
+        (df_t['Weekday'] == 5) & (df_t['Hour'] == 0),
+        (df_t['Weekday'] == 5) & (df_t['Hour'] == 23),
+        (df_t['Weekday'] == 5) & (df_t['Hour'] >= 8) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 5) & (df_t['Hour'] >= 1) & (df_t['Hour'] <= 7),
+        (df_t['Weekday'] == 5) & (df_t['Hour'] >= 19) & (df_t['Hour'] <= 22),
+
+        # domingo
+        (df_t['Weekday'] == 6) & (df_t['Hour'] >= 0) & (df_t['Hour'] <= 18),
+        (df_t['Weekday'] == 6) & (df_t['Hour'] == 19),
+        (df_t['Weekday'] == 6) & (df_t['Hour'] >= 20) & (df_t['Hour'] <= 21),
+        (df_t['Weekday'] == 6) & (df_t['Hour'] >= 22) & (df_t['Hour'] <= 23)
+    ]
+
+    opciones = [4, 4, 4, 5, 3, 3, 2,
+                4, 4, 4, 5, 3, 3, 1, 2,
+                4, 4, 4, 5, 3, 3, 1, 2,
+                4, 4, 4, 5, 3, 3, 1, 2,
+                4, 4, 4, 5, 3, 3, 1, 2,
+                4, 4, 4, 5, 3,
+                5, 4, 3, 4
+                ]
+
+    df_t['Block'] = np.select(condiciones,opciones)
+    df_t = df_t.drop(['Hour','Weekday'], axis=1)
+
+    return condiciones, opciones, df_t
---
+++ scripts/SDDP_Dispa-SET_mapping.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 16:02:04 2023
+
+@author: UMSS
+"""
+import dispa_link as dl
+#import get_database_SDDP as sd
+import pandas as pd
+import os
+
+start_date, end_date = '2025-12-31 23:00:00+00:00', '2026-12-31 23:00:00+00:00'
+
+#TODO: user shoudl only specify folder where SDDP database was extracted (becase datafiles are standrdized and always have the same names)
+
+#Inputs for alert level
+SDDP_alert = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/volale.csv', index_col=0, header=3)
+SDDP_alert.columns = SDDP_alert.columns.str.replace(' ', '')
+hydro = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/chidrobo.csv', usecols=['...Nombre...','.VMax..','.VInic.','....Pot','.FPMed.','C.Vert.','.PV.','.QMax..'], header=0)
+
+#Inputs demand
+SDDP_demand = pd.read_fwf('../../../Dispa-LINK/Inputs/SDDP/dbf005bo.dat', encoding='cp1252', header=0, colspecs="infer", engine='python')
+dfc = pd.read_fwf('../../../Dispa-LINK/Inputs/SDDP/dcirc.dat',header=1,colspecs="infer", engine='python', encoding='cp1252')
+buslist = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/BusList-ZONES1.csv', encoding='cp1252')
+#Inputs outages
+#thermal
+dft1 = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/pmtrsebo.csv', encoding='cp1252')
+dft2 = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/ctermibo.csv', usecols=['...Nombre...','..Ih...','.PotIns','Comb'])
+
+#hydro
+dfh1 = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/pmhisebo.csv', encoding='cp1252')
+#vres
+dfv1 = pd.read_fwf('../../../Dispa-LINK/Inputs/SDDP/cgndbo.dat', header=1, colspecs="infer", engine='python',usecols=['!Num Name........','ProbFal','.PotIns'])
+
+dbus = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/dbus.csv',encoding='cp1252', header=1, usecols= ['Name','Gen. name'])
+
+#Renewables availability factor
+vresaf = pd.read_fwf('../../../Dispa-LINK/Inputs/SDDP/blrenw005bo0_w.dat',header=1,colspecs="infer", engine='python')
+
+#reservoirlevel
+SDDP_lev = pd.read_csv('../../../Dispa-LINK/Inputs/SDDP/rvolfin.csv', encoding='cp1252', header=3)
+
+#ScaledInflows
+InflowsSDDP = pd.read_fwf('../../../Dispa-LINK/Inputs/SDDP/hinflw_w.dat', header=0, colspecs="infer", engine='python')
+
+#Results
+alertlev = dl.get_alert_level(hydro,SDDP_alert, start_date=start_date, end_date=end_date)
+finaldemand = dl.get_demand(SDDP_demand, dfc, buslist)
+ntc = dl.get_NTC(dfc)
+Outages = dl.get_Outages(dft1,dft2, dfh1,dfv1)
+PowerPlantData = dl.get_Power_PlantData(hydro,dft2,dfv1,dbus,buslist)
+RenewablesAF = dl.get_renewables_AF(vresaf)
+Spilcost = dl.get_Spillage_Cost(hydro)
+reserlevh = dl.get_Storage_lev(SDDP_lev,hydro)
+#Scaled Inflows
+path = "../../../Dispa-LINK/Outputs/SDDP/Database/Scaled_Inflows"
+os.makedirs(path)
+ScaledInflows = dl.get_Scaled_inflows(InflowsSDDP,hydro)
+
+#Outputs
+alertlev.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/AlertLevel.csv', header=True, index=True)
+finaldemand.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/Demand.csv') 
+ntc.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/NTC.csv')
+Outages.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/Outages.csv')
+PowerPlantData.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/PowerPlantData.csv') 
+RenewablesAF.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/RenewablesAF.csv')
+Spilcost.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/Spillage_cost.csv')
+reserlevh.to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/StorageLevel.csv', header=True, index=True)
+for year in range(1979, 2022):
+    ScaledInflows.loc[str(year)].to_csv('../../../Dispa-LINK/Outputs/SDDP/Database/Scaled_Inflows/' + str(year) + '.csv')
